6093c8deb254704ace0397ac71b487163558a487
GoopSoftware <jhreschke@gmail.com>
2025-04-03 22:58:05 -0600

    4/3 - Added first rendition of html

--- portfolio/portfolio_app/urls.py
+++ portfolio/portfolio_app/urls.py
@@ -3,11 +3,21 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
+
     path('students/', views.student, name='student'),
+    path('students/<int:id>/',
+         views.student_detail,
+         name='student-detail'),
+
     path('projects/', views.projects, name='projects'),
+    path('projects/<int:id>/',
+         views.project_detail,
+         name='project-detail'),
+
     path(
         'portfolio/<int:id>/',
         views.portfolio_detail,
         name='portfolio-detail'
     ),
+
 ]
--- portfolio/portfolio_app/views.py
+++ portfolio/portfolio_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-from .models import Portfolio, Student
+from .models import Portfolio, Student, Project
 
 # Create your views here.
 def index(request):
@@ -10,13 +10,52 @@ def index(request):
                   {'portfolios': portfolios})
 
 def student(request):
-    return render(request, 'portfolio_app/student.html')
+    student = Student.objects.all()
+    return render(request,
+                  'portfolio_app/student.html',
+                  {
+                      'students': student
+        }
+    )
+
+def student_detail(request, id):
+    student = get_object_or_404(Student, id=id)
+    portfolio = student.portfolio
+
+    return render(
+        request,
+        'portfolio_app/student_detail.html',
+        {
+            'student': student,
+            'portfolio': portfolio,
+            'projects': portfolio.projects.all()
+        }
+    )
 
 def projects(request):
-    return render(request, 'portfolio_app/projects.html')
+    projects = Project.objects.all()
+
+    return render(
+        request,
+        'portfolio_app/projects.html',
+        {
+            'projects': projects,
+        }
+    )
+
+def project_detail(request, id):
+    project = get_object_or_404(Project, id=id)
+
+    return render(
+        request,
+        'portfolio_app/project_detail.html',
+        {
+            'project': project
+        }
+
+    )
 
 def portfolio_detail(request, id):
-    #portfolio = get_object_or_404(Portfolio, id=id)
     student = get_object_or_404(Student, id=id)
     portfolio = student.portfolio
 
